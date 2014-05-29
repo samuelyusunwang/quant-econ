db8309542ccb7927038ee74921a7cbd29280ff28
samuelyusunwang <samuelyusunwang@gmail.com>
2014-05-29 15:48:27 -0500

    Test01
    Change a file

--- examples/white_noise_plot.py
+++ examples/white_noise_plot.py
@@ -2,6 +2,6 @@ from pylab import plot, show, legend
 from random import normalvariate
 
 x = [normalvariate(0, 1) for i in range(100)]
-plot(x, 'b-', label="white noise")
+plot(x, 'r-', label="white noise")
 legend()
 show()
